1b89966fb19ebee6e863110b7394fddd77c37397
joejohn98 <joejohn048@gmail.com>
2025-05-13 16:36:01 +0530

    Add projects data and define Project interface for portfolio

---
+++ src/data/projects.ts
@@ -0,0 +1,50 @@
+
+
+import type { Project } from '../types';
+
+export const projects: Project[] = [
+ {
+    id: 1,
+    title: 'Urban Threads',
+    description:
+      'A modern e-commerce platform built with React, TypeScript, and Redux, offering a seamless shopping experience for fashion enthusiasts.',
+    tags: ['React', 'TypeScript', 'Redux Toolkit', 'Tailwind CSS', 'API Integration' ,'React Router'],
+    demoUrl: 'https://your-demo-url.com/urban-threads',
+    githubUrl: 'https://github.com/your-username/urban-threads',
+    imageUrl: 'https://ik.imagekit.io/m2fpyi4uw/Projects/Urban-Threads.png?updatedAt=1746897118849',
+
+  },
+  {
+    id: 2,
+    title: 'Recipe Hub',
+    description:
+      'A feature-rich recipe management app that lets users add, organize, and favorite recipes with local persistence and a modern UI.',
+    tags: ['React', 'TypeScript', 'Tailwind CSS', 'React Router', 'Context API'],
+    demoUrl: 'https://your-demo-url.com/recipe-hub',
+    githubUrl: 'https://github.com/your-username/recipe-hub',
+    imageUrl: 'https://ik.imagekit.io/m2fpyi4uw/Projects/Recipe-Box.png?updatedAt=1746897119158'
+
+  },
+  {
+    id: 3,
+    title: 'Inventory Management App',
+    description:
+      'A responsive web application to manage products, stock levels, and departments, with sorting, filtering, and real-time updates.',
+    tags: ['React', 'Tailwind CSS', 'React Router', 'Context API', 'API Integration'],
+    demoUrl: 'https://your-demo-url.com/inventory-app',
+    githubUrl: 'https://github.com/your-username/inventory-management-app',
+    imageUrl: 'https://ik.imagekit.io/m2fpyi4uw/Projects/Inventory-Hub.png?updatedAt=1746897118339',
+
+  },
+  {
+    id: 4,
+    title: 'Destino - Travel Destination Explorer',
+    description:
+      'A beautifully designed travel destination explorer that lets users browse destinations by continent and country, with detailed info and global coverage.',
+    tags: ['React', 'TypeScript', 'Tailwind CSS', 'React Router DOM', 'Context API'],
+    demoUrl: 'https://your-demo-url.com/destino',
+    githubUrl: 'https://github.com/your-username/destino',
+    imageUrl: 'https://ik.imagekit.io/m2fpyi4uw/Projects/Destino-Travel-Desination.png?updatedAt=1746897118852'
+
+  }
+];
